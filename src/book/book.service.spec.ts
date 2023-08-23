c65331710cae5ceb8cbbe8375a8e0de3d0a0953b
davidas56 <davidas362@gmail.com>
2023-08-23 08:32:28 -0500

    con seguridad y JWT implementado y pruebas exitosa

--- src/book/book.service.spec.ts
+++ src/book/book.service.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { BookController } from './book.controller';
 import { BookService } from './book.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Book } from './book.entity';
 
-describe('BookService', () => {
-  let service: BookService;
+describe('BookController', () => {
+  let controller: BookController;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [BookService],
+    module = await Test.createTestingModule({
+      controllers: [BookController],
+      providers: [
+        BookService,
+        {
+          provide: getRepositoryToken(Book),
+          useValue: {}, // Puedes usar un objeto vacío como stub
+        },
+      ],
     }).compile();
 
-    service = module.get<BookService>(BookService);
+    controller = module.get<BookController>(BookController);
+  });
+
+  afterAll(async () => {
+    await module.close(); // Cierra el módulo después de las pruebas
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(controller).toBeDefined();
   });
+
+  // Agrega más pruebas aquí
+
 });
